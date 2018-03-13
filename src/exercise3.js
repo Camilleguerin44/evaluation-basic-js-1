d32fe2736a255d257a0b67ed03f5a405068cbb34
Guerini <camille.guerin@ynov.com>
2018-03-13 10:38:51 +0100

    Exercise3.js envoyé

--- src/exercise3.js
+++ src/exercise3.js
@@ -1,5 +1,23 @@
 var applyMap = function() {
-  // Your future job begins here ...
+function Premier() {
+  var output = [];
+  return output;
+}
+  function Deuxieme(){
+    var inputValues = [12, 26, 42];
+    const deux = inputValues.deux(x => '');
+  }
+  function troisieme(){
+    var inputMap=[map];
+    var inputValues = [12, 26, 42];
+    const trois = inputValues.trois(x=> x - 2);
+    var inputMap2 = inputMap.shift();
+    var trois = inputMap.push();
+  }
+  function quatrieme(){
+  var inputValues[5, 7, 18];  
+  const
+  }
 }
 
 module.exports = {
