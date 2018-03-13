5294932defd63e3083bb91720527f19bdc98fa34
Guerini <camille.guerin@ynov.com>
2018-03-13 10:37:51 +0100

    Exercise1.js fais

--- src/exercise1.js
+++ src/exercise1.js
@@ -1,6 +1,16 @@
 var removeNegativeValue = function(values) {
-  // Your future job begins here ...
+
+//var output = [];
+//Return output;
+
+function input(number){
+  return number >=0;
 }
+var output = [34, -5, 9, 18, 0, -7, 32].filter(input)
+  return output;
+
+}
+
 
 module.exports = {
   title: 'Exercise 1',
